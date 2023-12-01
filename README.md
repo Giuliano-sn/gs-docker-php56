58dc2ef6e84df05b6bcfad345f03bb8a18e30c18
Giuliano MacBookPro <giuliano@giusoft.com.br>
2023-12-01 17:30:32 +0000

    Melhorias na documentação

--- README.md
+++ README.md
@@ -1,12 +1,13 @@
 # Ambiente de programação Web da GiuSoft
-Para desenvolvimento Web com PHP 5.6.40 (com Apache) e MySQL 8.0.27
+Para desenvolvimento Web com PHP 5.6.40 (com Apache) e MySQL 8.0.34
 
-Este ambiente já vem pré-configurado com as características necessárias para o desenvolvimento web na GiuSoft, exemplo:
+Este ambiente já vem pré-configurado com as características necessárias para o desenvolvimento web na GiuSoft:
 
 - Endereços de IP que simulam o ambiente de produção
 - PHP com a versão de produção atual, e parâmetros modificados (short_open_tag, max_execution_time, memory_limit e error_reporting
 - Apache com configuração semelhante)
-- MySQL com a configuração adequada para interpretar corretamente os códigos de página (Collation e UTF)
+- MySQL com a configuração adequada para interpretar corretamente os códigos de página (Collation e UTF), com o usuário `web` criado, e já com as principais bases de dados instaladas (gadmin, webcfc_admin, webcfc_ba_giusoft e wms_giusoft)
+- 
 
 ## Requisitos
 
@@ -14,6 +15,11 @@ Este ambiente já vem pré-configurado com as características necessárias para
 - Você já deve ter uma conta corportativa no Github e seu ambiente local já deverá estar configurado.
 
 ## Instalação
+
+Do docker no macOS:
+
+`brew install --cask docker`
+
 
 Digite:
 ```
@@ -29,3 +35,8 @@ cd gs-docker-php56
 Web: localhost:8080
 MySQL: localhost:8036 (usuário root, sem senha)
 
+## Para utilizar
+
+Para iniciar o docker compose, digite no raiz deste projeto:
+
+`docker compose up -d`
--- instalar.sh
+++ instalar.sh
@@ -1,15 +1,74 @@
 #!/bin/bash
-sudo mkdir -p /var/www/html
+clear
+echo "********************* ATENÇÃO **********************"
+echo
+echo "Requisitos para instalação:"
+echo
+echo "- Docker corretamente instalado"
+echo "- Conta no Github corporativo configurada neste computador"
+echo
+echo "Tecle algo para prosseguir ou CTRL+C para interromper..."
+read -n 1
+
+if [ ! -d /var/www/html ];then
+    echo "- Criando pasta /var/www/html"
+    sudo mkdir -p /var/www/html
+else
+    echo "- Pasta /var/www/html já existe"
+fi
+# Criando containers
+cd bd
+docker stop mysql
+docker rm -f mysql
+docker build -t mysql .
+docker run -d --name mysql mysql
+docker stop mysql
+cd ..
+cd php56
+docker stop php56
+docker rm -f php56
+docker build -t php56 .
+docker run -d --name php56 php56
+docker stop php56
+cd ..
+cd php82
+docker stop php82
+docker rm -f php82
+docker build -t php82 .
+docker run -d --name php82 php82
+docker stop php82
+cd ..
+# Inicia todos os containers criados de uma vez
+docker-compose up -d
+# Alterando DNS para encontrar Docker MySQL
+docker exec -i php56 sh -c 'echo "10.0.0.12 bd-webcfc.giusoft.com.br" >> /etc/hosts'
+docker exec -i php56 sh -c 'echo "10.0.0.17 www.giusoft.com.br webcfc.com.br www.webcfc.com.br" >> /etc/hosts'
+# Carregando bases de dados principais
+cd bd
+docker exec -i mysql sh -c 'exec mysql -uroot webcfc_admin --execute "CREATE USER 'web'@'10.0.0.%' IDENTIFIED BY 'web';GRANT ALL PRIVILEGES ON * . * TO 'web'@'%';FLUSH PRIVILEGES;"' 
+docker exec -i mysql sh -c 'exec mysql -uroot webcfc_admin --execute "CREATE USER 'web'@'%' IDENTIFIED BY 'web';GRANT ALL PRIVILEGES ON * . * TO 'web'@'%';FLUSH PRIVILEGES;' 
+docker exec -i mysql sh -c 'exec mysql -uroot webcfc_admin' < webcfc_admin.sql
+docker exec -i mysql sh -c 'exec mysql -uroot webcfc_ba_giusoft' < webcfc_ba_giusoft.sql
+docker exec -i mysql sh -c 'exec mysql -uroot gadmin' < gadmin.sql
+docker exec -i mysql sh -c 'exec mysql -uroot wms_giusoft' < wms_giusoft.sql
+cd ..
+# Instalando script de OS
 sudo cd /var/www/html
 if [ ! -f /usr/local/bin/os ];then
+    echo "- Instalando arquivo os..."
     cp -f os /usr/local/bin
 fi
+# Trazendo repositórios
 if [ ! -d /var/www/html/gfw ];then
+    echo "- Instalando repo gfw..."
     git clone https://github.com/giusoft/gfw
 fi
 if [ ! -d /var/www/html/webcfc ];then
+    echo "- Instalando repo webcfc..."
     git clone https://github.com/giusoft/webcfc
 fi
 if [ ! -d /var/www/html/wms ];then
+    echo "- Instalando repo wms..."
     git clone https://github.com/giusoft/wms
 fi
+
